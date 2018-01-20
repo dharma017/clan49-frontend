47b472df55729cc6b5eaac36a1c06ff1cb540ae6
Dharmaraj Thapa <dharma.thapa@stuxbot.com>
2018-01-20 09:24:16 +0545

    Server rendering fix for production

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
 
 app.prepare()
     .then(() => {
@@ -12,17 +14,17 @@ app.prepare()
         server.get('/p/:id', (req, res) => {
             const actualPage = '/player'
             const queryParams = { id: req.params.id }
-            app.render(req, res, actualPage, queryParams)
+            return app.render(req, res, actualPage, queryParams)
         })
 
         server.get('*', (req, res) => {
             return handle(req, res)
         })
 
-        server.listen(3000, (err) => {
-            if (err) throw err
-            console.log('> Ready on http://localhost:3000')
-        })
+        server.listen(port, err => {
+          if (err) throw err;
+          console.log(`> Ready on http://localhost:${port}`);
+        });
     })
     .catch((ex) => {
         console.error(ex.stack)
