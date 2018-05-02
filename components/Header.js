516d67dae37ae5388e0485c48b166d5e42830c0b
Dharma R. Thapa <dharma.thapa@stuxbot.com>
2018-05-02 16:22:11 +0545

    Update clan war for cr

--- components/Header.js
+++ components/Header.js
@@ -8,47 +8,58 @@ const linkStyle = {
   marginRight: 15
 }
 
-const Header = () => (
-  <div id="menu">
-    <Menu
-      // onClick={this.handleClick}
-      // selectedKeys={[this.state.current]}
-      mode="horizontal"
-    >
+class Header extends React.Component {
+  state = {
+    current: 'cr',
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
+  render() {
+    return (
+      <Menu
+        // onClick={this.handleClick}
+        // selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
 
-      <SubMenu title={<span>Clash Royale</span>}>
-        <Menu.Item key="cr">
-          <Link href="/">
-            <a style={linkStyle}>Clan</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="clanwar">
-          <Link href="/clanwar">
-            <a style={linkStyle}>War Log</a>
-          </Link>
-        </Menu.Item>
-      </SubMenu>
+        <SubMenu title={<span>Clash Royale</span>}>
+          <Menu.Item key="cr">
+            <Link href="/">
+              <a style={linkStyle}>Clan</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="clanwar">
+            <Link href="/clanwar">
+              <a style={linkStyle}>War Log</a>
+            </Link>
+          </Menu.Item>
+        </SubMenu>
 
-      <SubMenu title={<span>Clash of Clans</span>}>
-        <Menu.Item key="coc">
-          <Link href="/coc">
-            <a style={linkStyle}>Clan</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="currentwar">
-          <Link href="/currentwar">
-            <a style={linkStyle}>Current War</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="warlog">
-          <Link href="/warlog">
-            <a style={linkStyle}>War Log</a>
-          </Link>
-        </Menu.Item>
-      </SubMenu>
+        <SubMenu title={<span>Clash of Clans</span>}>
+          <Menu.Item key="coc">
+            <Link href="/coc">
+              <a style={linkStyle}>Clan</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="currentwar">
+            <Link href="/currentwar">
+              <a style={linkStyle}>Current War</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="warlog">
+            <Link href="/warlog">
+              <a style={linkStyle}>War Log</a>
+            </Link>
+          </Menu.Item>
+        </SubMenu>
 
-    </Menu>
-  </div>
-);
+      </Menu>
+    );
+  }
+}
 
 export default Header
